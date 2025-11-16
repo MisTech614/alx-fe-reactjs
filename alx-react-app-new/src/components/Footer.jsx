e890abac00fa170532bed438d973a8786734ceb9
dafrosambanga <dafrosambanga@gmail.com>
2025-11-16 19:29:45 +0300

    style-1b

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,15 +1,16 @@
 function Footer() {
-    return (
-      <footer style={{
+  return (
+    <footer
+      style={{
         backgroundColor: "#222",
         color: "white",
         textAlign: "center",
         padding: "15px",
         marginTop: "20px",
-      }}>
-        <p style={{ margin: 0 }}>© 2023 City Lovers</p>
-      </footer>
-    );
+      }}
+    >
+      <p style={{ margin: 0 }}>© 2025 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
 }
 export default Footer;
-  
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,24 +1,30 @@
-const UserProfile = (props) => {
-    return (
-      <div style={{
+function UserProfile(props) {
+  return (
+    <div
+      style={{
         border: "1px solid lightgray",
         padding: "15px",
         margin: "15px",
         borderRadius: "8px",
         backgroundColor: "#f9f9f9",
-      }}>
-        <h2 style={{ color: "blue", marginBottom: "5px" }}>{props.name}</h2>
-        <p style={{ margin: "5px 0" }}> 
-          Age:{" "}
-         <span style={{ fontWeight: "bold", color: "darkslategray" }}>
+      }}
+    >
+      <h2 style={{ color: "blue", marginBottom: "5px" }}>{props.name}</h2>
+
+      <p style={{ margin: "5px 0" }}>
+        Age:{" "}
+        <span style={{ fontWeight: "bold", color: "darkslategray" }}>
           {props.age}
-          </span>
-        </p>
-        <p style={{ margin: "5px 0", lineHeight: 1.5 }}>
-          <strong>Bio:</strong> {props.bio}
-          </p>
-      </div>
-    );
-};
+        </span>
+      </p>
+
+      <p style={{ margin: "5px 0", lineHeight: 1.5 }}>
+        <strong>Bio:</strong> {props.bio}
+      </p>
+    </div>
+  );
+}
 export default UserProfile;
+
+
   
