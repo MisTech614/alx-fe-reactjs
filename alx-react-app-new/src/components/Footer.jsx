c38c3f85ed40a8a1c11844b34f8d0a69e432354f
dafrosambanga <dafrosambanga@gmail.com>
2025-11-16 19:54:20 +0300

    inline styling

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,10 +1,19 @@
 
 function Footer() {
   return (
-    <footer style={{ backgroundColor: 'black', color: 'white', padding: '15px', textAlign: 'center' }}>
-      <p>© 2025 MyApp</p>
+    <footer
+      style={{
+        backgroundColor: 'black',
+        color: 'white',
+        textAlign: 'center',
+        padding: '10px',
+        marginTop: '20px',
+      }}
+    >
+      <p>© 2025 My Favorite Cities</p>
     </footer>
   );
 }
 export default Footer;
+
 
