f92b63439619cfcb96b2c819b58786378291231f
dafrosambanga <dafrosambanga@gmail.com>
2025-11-30 21:19:43 +0300

    updates-8 on Github API project task

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import "./index.css";
 import SearchBar from "./components/SearchBar";
 import UserCard from "./components/UserCard";
-import "./index.css";
 import Search from "./components/Search";
 
 function App() {
